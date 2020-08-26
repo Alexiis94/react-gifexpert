900f1f544102f0cc42ac559113208f2b6c7025d2
Pool <alexiis.95.15@gmail.com>
2020-08-26 15:50:19 -0400

    Docs Agregado

---
+++ docs/precache-manifest.2e3d17d09413c17e66ea7c8995f29534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd52c9a0bca277de326cb8874281576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e88f52f58b1893adf32",
+    "url": "/static/css/main.fcd034b9.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e88f52f58b1893adf32",
+    "url": "/static/js/main.acc244fd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
